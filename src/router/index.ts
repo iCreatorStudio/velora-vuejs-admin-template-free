4f53ee59c444ab9a928938380f9ac6fe7629d315
iCreatorStudio <icreatorstudio9@gmail.com>
2025-01-29 14:18:55 +0530

    Fix router history configuration to use BASE_URL

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   scrollBehavior: () => {
     // always scroll to top
     return { top: 0, behavior: 'smooth' }
